9cb67cee7dd046b4c5924e026141314fc212b587
KaiPaga <165836741+KaiPaga@users.noreply.github.com>
2024-06-06 19:25:47 -0700

    Update bottom section.js

--- js/bottom section.js
+++ js/bottom section.js
@@ -4,21 +4,21 @@
 */
 
 /* When the user clicks on the button,
-toggle between hiding and showing the dropdown content*/
+toggle between hiding and showing the dropdown content */
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = fucntion(event) {
-if (!event.target.matches( '.dropbtn')) {
-  var dropdowns = document.getElementsByClssName("dropdown-content");
-  var i;
-  for (i = 0; i < dropdowns. lengths; i++) {
-    var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contain('show')) {
-      openDropdown.classList.remove('show');
+// Close the dropdown menu if the user clicks outside of it 
+window.onclick = function(event) {
+    if (!event.target.matches( '.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns. lengths; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDopdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
-  }
-}
 }
