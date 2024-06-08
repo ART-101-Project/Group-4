c967cc739e7f47505cbe64be74d57387873c0bdf
KaiPaga <165836741+KaiPaga@users.noreply.github.com>
2024-06-08 15:01:53 -0700

    Update bottom section.js

--- js/bottom section.js
+++ js/bottom section.js
@@ -3,4 +3,11 @@
 *Created: June 4th
 */
 
-
+function toggleText(elementId) {
+  var element = document.getElementById(elementId);
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
