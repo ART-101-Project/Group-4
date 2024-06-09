23d69a22a12d5a3a8823286320390965e0d05fd9
KaiPaga <165836741+KaiPaga@users.noreply.github.com>
2024-06-08 17:29:56 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-function toggleText(elementId) {
-  var element = document.getElementById(elementId);
-  if (element.style.display === "none") {
-    element.style.display = "block";
-  } else {
-    element.style.display = "none";
-  }
-}
+document.addEventListener("DOMContentLoaded", function() {
+    // Get all buttons
+    var buttons = document.querySelectorAll('.toggle-btn');
+    
+    // Attach click event listener to each button
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            // Toggle the display of the next sibling element (which is the additional text)
+            var textToShow = this.nextElementSibling;
+            toggleText(textToShow);
+        });
+    });
+
+    // Function to toggle the display of an element
+    function toggleText(element) {
+        if (element.style.display === "none") {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
+    }
+});
